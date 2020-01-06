698296563016c258c53f91abb4716fb39471d7c8
mukesh22584 <mks22584@gmail.com>
2020-01-06 18:31:31 +0530

    sm8150-common: don't ENFORCE_RRO_TARGETS

--- common.mk
+++ common.mk
@@ -21,10 +21,6 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-xtended
 
-PRODUCT_ENFORCE_RRO_TARGETS := *
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-xtended/packages/apps/Snap
-
 # A/B
 AB_OTA_UPDATER := true
 
